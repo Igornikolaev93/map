b66d941b3134b6dfa9394427af1479eb57bdc9bc
Игорь Николаев <goscha.nikolae2015@yandex.ru>
2026-08-14 17:57:02 +0300

    sa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,59 +23,29 @@ export default defineNuxtConfig({
     }
   },
 
+  // ВАЖНО: Для разработки - корень, для GitHub Pages - /map/
   app: {
     baseURL: isDev ? '/' : (isGithubPages ? '/map/' : '/'),
     head: {
       meta: [
         { charset: 'utf-8' },
-        // Основной viewport
         { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
-        
-        // Для PWA на мобильных (современный стандарт)
         { name: 'mobile-web-app-capable', content: 'yes' },
-        
-        // Для iOS (все еще поддерживается, но лучше использовать вместе с mobile-web-app-capable)
         { name: 'apple-mobile-web-app-capable', content: 'yes' },
-        
-        // Цвет темы для браузера
         { name: 'theme-color', content: '#0a0a1a' },
-        
-        // Стиль статус-бара для iOS
-        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
-        
-        // Отключение автоопределения телефонов
-        { name: 'format-detection', content: 'telephone=no' },
-        
-        // Описание для SEO
-        { name: 'description', content: 'Интерактивная гибридная карта путешествий с 3D глобусом' }
+        { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }
       ],
       link: [
         {
           rel: 'stylesheet',
           href: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css'
-        },
-        {
-          rel: 'icon',
-          type: 'image/x-icon',
-          href: '/favicon.ico'
-        },
-        {
-          rel: 'apple-touch-icon',
-          href: '/apple-touch-icon.png'
-        },
-        {
-          rel: 'manifest',
-          href: '/manifest.json'
         }
       ],
       script: [
         {
           src: 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
         }
-      ],
-      htmlAttrs: {
-        lang: 'ru'
-      }
+      ]
     }
   },
 
